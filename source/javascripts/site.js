13284d9d99305ded1894cf3d46b3431e270fe0fb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-12 10:51:11 -0700

    Update movement and colors

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,11 @@
 // This is where it all goes :)
   const trackable = document.querySelector('.trackable')
-  const upperL = document.querySelector('.left-upper')
-  const name = upperL.querySelector('.name')
+  const name = document.querySelector('.name')
+  const connect = document.querySelector('.connect')
   const movement = 50
 
   function shadow(e) {
-    const { offsetWidth: width, offsetHeight: height } = upperL
+    const { offsetWidth: width, offsetHeight: height } = trackable
     let { offsetX: x, offsetY: y } = e
 
     if (this !== e.target) {
@@ -17,8 +17,10 @@
     const yMovement = Math.round((y / width * movement) - (movement / 2))
 
     name.style.textShadow = `
-      ${xMovement}px ${yMovement}px 0 aqua,
-      ${xMovement * -1}px ${yMovement * -1}px 0 lightgreen`
+      ${xMovement}px ${yMovement}px 0 orange,
+      ${xMovement + 25 }px ${yMovement * -1 + 25}px 0 red`
+    connect.style.textShadow = `
+      ${xMovement }px ${yMovement * -2 + 25}px 0 red`
   }
 
   trackable.addEventListener('mousemove', shadow)
