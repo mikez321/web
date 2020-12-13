4148705addaa3c9f86560951ccbf75e94645d9eb
Mike Hernandez <hernandezmichael321@gmail.com>
2020-12-13 08:14:09 -0700

    Add initial text shadow and transition to shadow movement

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,22 +1,22 @@
 // This is where it all goes :)
-  const trackable = document.querySelector('.trackable')
-  const name = document.querySelector('.name')
-  const movement = 50
+const trackable = document.querySelector('.trackable')
+const name = document.querySelector('.name')
+const movement = 50
 
-  function shadow(e) {
-    const { offsetWidth: width, offsetHeight: height } = trackable
-    let { offsetX: x, offsetY: y } = e
+function shadow(e) {
+  const { offsetWidth: width, offsetHeight: height } = trackable
+  let { offsetX: x, offsetY: y } = e
 
-    if (this !== e.target) {
-      x = x + e.target.offsetLeft
-      y = y + e.target.offsetTop
-    }
-    const xMovement = Math.round((x / width * movement) - (movement / 2))
-    const yMovement = Math.round((y / width * movement) - (movement / 2))
-
-    name.style.textShadow = `
-      ${xMovement}px ${yMovement}px 0 orange,
-      ${xMovement + 25 }px ${yMovement * -1 + 25}px 0 red`
+  if (this !== e.target) {
+    x = x + e.target.offsetLeft
+    y = y + e.target.offsetTop
   }
+  const xMovement = Math.round((x / width * movement) - (movement / 2))
+  const yMovement = Math.round((y / width * movement) - (movement / 2))
 
-  trackable.addEventListener('mousemove', shadow)
+  name.style.textShadow = `
+    ${xMovement}px ${yMovement}px 0 orange,
+    ${xMovement + 25 }px ${yMovement * -1 + 25}px 0 red`
+}
+
+trackable.addEventListener('mousemove', shadow)
